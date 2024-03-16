f18da9f36e40d81fb1ab6099ae4ba7fcb4e0c307
BlasterRiv <153321200+BlasterRiv@users.noreply.github.com>
2024-03-16 11:57:25 +0000

    Feeature room Effect

--- src/Feature.java
+++ src/Feature.java
@@ -1,2 +1,16 @@
 public class Feature {
+    private String title;
+    private Effect effect;
+    private String discription;
+
+    public String getTitle() {return title;}
+    public Effect getEffect(){return effect;}
+    public String getDiscription() {return discription;}
+
+
+    public Feature(String ti, Effect ef, String discrip) {
+        title = ti;
+        effect = ef;
+        discription = discrip;
+    }
 }
--- src/Room.java
+++ src/Room.java
@@ -1,8 +1,17 @@
 import java.util.ArrayList;
 
 public class Room {
-    private boolean isCleared;
-    private int numOfThreats;
+
+
+    private boolean isLocked;
+
+    private boolean isNorthRoom;
+    private boolean isEastRoom;
+    private boolean isSouthRoom;
+    private boolean isWestRoom;
+
 
     private ArrayList<Enemy> ListOfEnemy = new ArrayList<Enemy>();
+    private int numOfThreats;
+    private boolean isCleared;
 }
